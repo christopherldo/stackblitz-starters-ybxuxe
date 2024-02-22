24709fd14b4ee2266a1b180a0ca109e68b19ea16
Christopher de Oliveira <christopherldo@gmail.com>
2024-02-22 15:24:05 -0400

    Setup test cases for Message component

---
+++ components/Message.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import Message from "./Message";
+
+describe("Message", () => {
+  const mockMessageSent = {
+    id: "test-id",
+    user_id: "test-id",
+    time: new Date(),
+    type: "sent",
+    content: "Test message sent",
+  } as MessageType;
+
+  const mockMessageReceived = {
+    ...mockMessageSent,
+    type: "received",
+  } as MessageType;
+
+  const mockUser = {
+    id: "test-id",
+    name: "Test User",
+    username: "test-user",
+    avatar_url: "https://test.com/avatar.jpg",
+  };
+
+  it("renders the sent message correctly", () => {
+    const { getByText } = render(<Message message={mockMessageSent} />);
+
+    expect(getByText(mockMessageSent.content)).toBeInTheDocument();
+  });
+
+  it("renders the received message with user avatar and name correctly", () => {
+    const { getByText, getByAltText } = render(
+      <Message message={mockMessageReceived} user={mockUser} />,
+    );
+
+    expect(getByText(mockMessageReceived.content)).toBeInTheDocument();
+    expect(
+      getByAltText(`${mockUser.name} avatar.`).getAttribute("src"),
+    ).toContain(encodeURIComponent(mockUser.avatar_url));
+  });
+});
